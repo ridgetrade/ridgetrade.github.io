d7b338db7986736c78fcad50c3a96ebc2c28b997
ridgetrade <johntomalone@gmail.com>
2021-10-06 16:54:54 +0800

    1006
    1006

--- js/jquery.js
+++ js/jquery.js
@@ -1,7 +1,195 @@
 $(function() {
+
+    var $goods = $('.goods1,.goods2,.goods3,.goods4,.goods5,.goods6,.goods7,.goods8,.goods9,.goods10,.goods11,.goods12,.goods13,.goods14,.goods15,.goods16,.goods17,.goods18,#content2');
+    var $menu = $('.menu1,.menu2,.menu3,.menu4,.menu5,.menu6,.menu7,.menu8,.menu9,.menu10,.menu11,.menu12,.menu13,.menu14,.menu15,.menu16,.menu17,.menu18')
+
+    $goods.hide();
+    $("#content2").show();
+    $("form,.badge,#shop").hide();
+
+    $(".menu1").click(function() {
+        $goods.fadeOut(300);
+        $(".goods1,.badge")
+            .delay(300)
+            .fadeIn(250);
+        $menu.removeClass("active");
+        $(this).addClass("active");
+    });
+
+    $(".menu2").click(function() {
+        $goods.fadeOut(300);
+        $(".goods2,.badge")
+            .delay(300)
+            .fadeIn(250);
+        $menu.removeClass("active");
+        $(this).addClass("active");
+    });
+
+    $(".menu3").click(function() {
+        $goods.fadeOut(300);
+        $(".goods3,.badge")
+            .delay(300)
+            .fadeIn(250);
+        $menu.removeClass("active");
+        $(this).addClass("active");
+    });
+
+    $(".menu4").click(function() {
+        $goods.fadeOut(300);
+        $(".goods4,.badge")
+            .delay(300)
+            .fadeIn(250);
+        $menu.removeClass("active");
+        $(this).addClass("active");
+    });
+
+    $(".menu5").click(function() {
+        $goods.fadeOut(300);
+        $(".goods5,.badge")
+            .delay(300)
+            .fadeIn(250);
+        $menu.removeClass("active");
+        $(this).addClass("active");
+    });
+
+    $(".menu6").click(function() {
+        $goods.fadeOut(300);
+        $(".goods6,.badge")
+            .delay(300)
+            .fadeIn(250);
+        $menu.removeClass("active");
+        $(this).addClass("active");
+    });
+
+    $(".menu7").click(function() {
+        $goods.fadeOut(300);
+        $(".goods7,.badge")
+            .delay(300)
+            .fadeIn(250);
+        $menu.removeClass("active");
+        $(this).addClass("active");
+    });
+
+    $(".menu8").click(function() {
+        $goods.fadeOut(300);
+        $(".goods8,.badge")
+            .delay(300)
+            .fadeIn(250);
+        $menu.removeClass("active");
+        $(this).addClass("active");
+    });
+
+    $(".menu9").click(function() {
+        $goods.fadeOut(300);
+        $(".goods9,.badge")
+            .delay(300)
+            .fadeIn(250);
+        $menu.removeClass("active");
+        $(this).addClass("active");
+    });
+
+    $(".menu10").click(function() {
+        $goods.fadeOut(300);
+        $(".goods10,.badge")
+            .delay(300)
+            .fadeIn(250);
+        $menu.removeClass("active");
+        $(this).addClass("active");
+    });
+
+    $(".menu11").click(function() {
+        $goods.fadeOut(300);
+        $(".goods11,.badge")
+            .delay(300)
+            .fadeIn(250);
+        $menu.removeClass("active");
+        $(this).addClass("active");
+    });
+
+    $(".menu12").click(function() {
+        $goods.fadeOut(300);
+        $(".goods12,.badge")
+            .delay(300)
+            .fadeIn(250);
+        $menu.removeClass("active");
+        $(this).addClass("active");
+    });
+
+    $(".menu13").click(function() {
+        $goods.fadeOut(300);
+        $(".goods13,.badge")
+            .delay(300)
+            .fadeIn(250);
+        $menu.removeClass("active");
+        $(this).addClass("active");
+    });
+
+    $(".menu14").click(function() {
+        $goods.fadeOut(300);
+        $(".goods14,.badge")
+            .delay(300)
+            .fadeIn(250);
+        $menu.removeClass("active");
+        $(this).addClass("active");
+    });
+
+
+    $(".menu15").click(function() {
+        $goods.fadeOut(300);
+        $(".goods15,.badge")
+            .delay(300)
+            .fadeIn(250);
+        $menu.removeClass("active");
+        $(this).addClass("active");
+    });
+
+    $(".menu16").click(function() {
+        $goods.fadeOut(300);
+        $(".goods16,.badge")
+            .delay(300)
+            .fadeIn(250);
+        $menu.removeClass("active");
+        $(this).addClass("active");
+    });
+
+    $(".menu17").click(function() {
+        $goods.fadeOut(300);
+        $(".goods17,.badge")
+            .delay(300)
+            .fadeIn(250);
+        $menu.removeClass("active");
+        $(this).addClass("active");
+    });
+
+    $(".menu18").click(function() {
+        $goods.fadeOut(300);
+        $(".goods18,.badge")
+            .delay(300)
+            .fadeIn(250);
+        $menu.removeClass("active");
+        $(this).addClass("active");
+    });
+
+    var $images = $('#images .btn')
+
+    $images.on('click', function() {
+        $(this).find('.plus').stop(true).animate({ left: ['70px', 'swing'], opacity: '100' }, 250, function() {
+            $(this).stop(true).animate({ left: '52px', opacity: '0' }, 250);
+        });
+    });
+
+    // var $images = $('#images .btn')
+
+    // $images.on('click', function () {
+    //     find('.plus').stop(true).animate({ left: ['50px', 'swing'], opacity: '100' }, 200, function () {
+    //     find('.plus').stop(true).animate({ left: '18px', opacity: '0' }, 250);
+    //     });
+    // });
+
     var duration = 200;
 
     // images ----------------------------------------
+
     var $images = $('#images p');
 
     $images
@@ -9,47 +197,11 @@ $(function() {
             $(this).find('strong').stop(true).animate({ bottom: '-70px' }, duration);
             $(this).find('.img2').stop(true).animate({ opacity: '1' }, duration);
         })
-
         .on('mouseout', function() {
             $(this).find('strong').stop(true).animate({ bottom: '0px' }, duration);
             $(this).find('.img2').stop(true).animate({ opacity: '0' }, duration);
         })
-
         .on('click', function() {
             $(this).find('strong').stop(true).animate({ bottom: '0px' }, duration);
         });
-
-      
-    // $images
-    // .on('mouseover', function() {
-    //     $(this).find('strong').stop(true).animate({ bottom: '0px' }, duration);
-    //     $(this).find('.img .img1').stop(true).animate({ opacity: 0 }, duration);
-    //     $(this).find('.img .img2').stop(true).animate({ opacity: 1 }, duration);
-    // })
-    // .on('mouseout', function() {
-    //     $(this).find('strong').stop(true).animate({ bottom: '-60px' }, duration);
-    //     $(this).find('.img .img1').stop(true).animate({ opacity: 1 }, duration);
-    //     $(this).find('.img .img2').stop(true).animate({ opacity: 0 }, duration);
-    // });
-
-    // $(".dropdown-menu a").click(function (e) {
-    //     e.preventDefault();
-    //     var url = this.href;
-
-    //     $("#content2")
-    //         .hide()
-    //         .load(url)
-    //         .delay(100)
-    //         .fadeIn(200)
-
-    // });
-
-    //   $images.filter(':nth-child(1)')
-    //   .on('mouseover', function(){
-    //       $(this).find('strong, span').stop(true).animate({opacity: 1}, duration);
-    //   })
-    //   .on('mouseout', function(){
-    //       $(this).find('strong, span').stop(true).animate({opacity: 0}, duration);
-    //   });
-
 });
